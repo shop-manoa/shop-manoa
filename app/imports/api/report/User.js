38df55e25895dc6a707f405e51c8007b0d227288
marusuke <ay26@hawaii.edu>
2024-04-06 18:23:34 -1000

    edited ReportUser
    - created a directory in ../imports/api called report which has users and post
    
    - ideas for simple schema for reporting form

---
+++ app/imports/api/report/User.js
@@ -0,0 +1,37 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/**
+ * The UsersCollection.
+ * It encapsulates state and variable values for users.
+ * It stores the info of the inappropriate users.
+ */
+class UsersCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'UsersCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      // reasons to categorize the user as inappropriate
+      // details about how the user is inappropriate against the community standards
+      // reasons{
+      //   type: Button or Choice
+      //   allowedValues: ['inappropriate', 'spam', 'offensive', 'other'],}.isRequired(),
+      //   },
+      // details: longString.isRequired(),
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the UsersCollection.
+ * @type {UsersCollection}
+ */
+export const Users = new UsersCollection();
