aae0981e0b87a9f3681d453d15135695a1e6e34c
JBallesteros <jay.d.awsome@gmail.com>
2024-04-10 22:09:47 -1000

    WIP ListItems page

--- app/imports/ui/pages/ListItems.jsx
+++ app/imports/ui/pages/ListItems.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ListItems = ({ item }) => (
-  <div className="list-item">
-    <img src={item.picture} alt={item.name} className="item-picture" />
-    <div className="item-details">
-      <div className="item-price">Price: ${item.price}</div>
-      <div className="item-condition">Condition: {item.condition}</div>
-      <div className="item-description">Description: {item.description}</div>
+const ListItems = ({ item }) => {
+  if (!item) {
+    return null; // or return a placeholder, error message, etc.
+  }
+
+  return (
+    <div className="list-item">
+      <img src={item.picture} alt={item.name} className="item-picture" />
+      <div className="item-details">
+        <div className="item-price">Price: ${item.price}</div>
+        <div className="item-condition">Condition: {item.condition}</div>
+        <div className="item-description">Description: {item.description}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ListItems.propTypes = {
   item: PropTypes.shape({
     picture: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     condition: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-  }).isRequired,
+    description: PropTypes.string.isRequired
+  }),
 };
 
 export default ListItems;
