31044a4e1a60d7f419c8bbdd254fc305ef0069a1
JBallesteros <jay.d.awsome@gmail.com>
2024-04-18 15:27:09 -1000

    Update ListItems.jsx

--- app/imports/ui/pages/ListItems.jsx
+++ app/imports/ui/pages/ListItems.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
-import ItemListing from '../components/ItemListing';
 import { ItemsList } from '../../api/items/ListItems'; // Import the ItemsList collection
 
 const ListItems = () => {
@@ -18,19 +17,31 @@ const ListItems = () => {
   }, []);
 
   return (
-      ready ? (
-          <Container className="py-3">
+    ready ? (
+      <Container className="py-3">
+        <Row className="justify-content-center">
+          <Col md={12}>
             <Row className="justify-content-center">
-              <Col md={12}>
-                <Row>
-                  {stuffs.map((stuff) => (
-                      <ItemListing key={stuff._id} stuff={stuff} />
-                  ))}
-                </Row>
-              </Col>
+              {stuffs.map((stuff) => (
+                <Col key={stuff._id} md={4}>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>{stuff.title}</Card.Title>
+                      <Card.Text>{stuff.description}</Card.Text>
+                      <img src={stuff.image} alt={stuff.title} style={{ width: '100px', height: '100px' }} />
+                      <Card.Text>Category: {stuff.category}</Card.Text>
+                      <Card.Text>Condition: {stuff.condition}</Card.Text>
+                      <Card.Text>Price: ${stuff.price}</Card.Text>
+                      <Card.Text>Owner: {stuff.owner}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
             </Row>
-          </Container>
-      ) : <LoadingSpinner />
+          </Col>
+        </Row>
+      </Container>
+    ) : <LoadingSpinner />
   );
 };
 
