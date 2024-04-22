6cdfbf458fd7889e9291849a04a33ac90cb56cc9
marusuke <ay26@hawaii.edu>
2024-04-21 17:51:34 -1000

    add links to each pages

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import { Image, Col, Row, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-light">
+  <footer className="mt-auto py-3 bg-info-subtle">
+    {/* You can change the color whatever you think it fits */}
     <Container>
       <Row>
-        {/* Make 3 Cols 1)Logo, 2)Menu, and 3)website info */}
-        <Col className="justify-content-lg-start">
+        {/* Make 4 Cols 1)Logo, 2)Menu, and 3)website info */}
+        <Col className="justify-content-sm-start">
           <Image roundedCircle src="/images/uhLogo.png" width="130px" />
+          {/*  Another choice for logo: <Image src="/images/uhLogo2.jpg" className="img-fluid" /> */}
         </Col>
-        <Col className="text-content">
-          Department of Information and Computer Sciences
-          {' '}
-          <br />
-          University of Hawaii
-          <br />
-          Honolulu, HI 96822
-          {' '}
-          <br />
-          <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
-            Template Home
-            Page
-          </a>
+        <Col className="justify-content-md-center">
+          <Col className="py-md-1"><Link to="/home">Home</Link></Col>
+          <Col className="py-md-1"><Link to="/list">Seller Profiles</Link></Col>
+          <Col className="py-md-1"><Link to="/items">Items</Link></Col>
+          <Col className="py-md-1"><Link to="/adminHome">Admin</Link></Col>
         </Col>
         <Col className="justify-content-lg-end">
-          <Image src="/images/uhLogo2.jpg" className="img-fluid" />
+          <h3>Shop Manoa</h3>
+          <p>__A brief info here__</p>
         </Col>
       </Row>
     </Container>
