eab159bc127ed78c56b7b9f2387e00b61ef1939d
JBallesteros <jay.d.awsome@gmail.com>
2024-04-25 12:39:37 -1000

    Update ListItems.jsx

--- app/imports/ui/pages/ListItems.jsx
+++ app/imports/ui/pages/ListItems.jsx
@@ -5,7 +5,6 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { ItemsList } from '../../api/items/ListItems';
-// import ItemListing from './ItemListing'; Import ItemListing component
 
 const ListItems = () => {
   const { stuffs, ready } = useTracker(() => {
@@ -19,32 +18,32 @@ const ListItems = () => {
   }, []);
 
   return (
-    ready ? (
-      <Container id="items-page" className="py-3">
-        <Row className="justify-content-center">
-          <Col md={12}>
+      ready ? (
+          <Container id="items-page" className="py-3">
             <Row className="justify-content-center">
-              {stuffs.map((stuff) => (
-                <Col key={stuff._id} md={4}>
-                  <Card>
-                    <Card.Body>
-                      <Card.Title>{stuff.title}</Card.Title>
-                      <Card.Text>{stuff.description}</Card.Text>
-                      <img src={stuff.image} alt={stuff.title} style={{ width: '100px', height: '100px' }} />
-                      <Card.Text>Category: {stuff.category}</Card.Text>
-                      <Card.Text>Condition: {stuff.condition}</Card.Text>
-                      <Card.Text>Price: ${stuff.price}</Card.Text>
-                      <Card.Text>Owner: {stuff.owner}</Card.Text>
-                      <Link to="../addReport" className="btn btn-outline-danger btn-sm custom-button" style={{ marginLeft: '10px' }}>report</Link>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
+              <Col md={12}>
+                <Row className="justify-content-center">
+                  {stuffs.map((stuff) => (
+                      <Col key={stuff._id} md={4}>
+                        <Card>
+                          <Card.Body>
+                            <Card.Title>{stuff.title}</Card.Title>
+                            <Card.Text>{stuff.description}</Card.Text>
+                            <img src={stuff.image} alt={stuff.title} style={{ width: '100px', height: '100px' }} />
+                            <Card.Text>Category: {stuff.category}</Card.Text>
+                            <Card.Text>Condition: {stuff.condition}</Card.Text>
+                            <Card.Text>Price: ${stuff.price}</Card.Text>
+                            {/* Link to the user's profile page */}
+                            <Link to={`/profile/${stuff.owner}`} className="btn btn-outline-primary btn-sm custom-button" style={{ marginLeft: '10px' }}>View Profile</Link>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                  ))}
+                </Row>
+              </Col>
             </Row>
-          </Col>
-        </Row>
-      </Container>
-    ) : <LoadingSpinner />
+          </Container>
+      ) : <LoadingSpinner />
   );
 };
 
