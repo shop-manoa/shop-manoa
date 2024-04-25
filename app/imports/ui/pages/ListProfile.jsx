566d9679ea1ac8ca661d44b05da249986f8c235b
JBallesteros <jay.d.awsome@gmail.com>
2024-04-25 12:53:01 -1000

    Update ListProfile.jsx

--- app/imports/ui/pages/ListProfile.jsx
+++ app/imports/ui/pages/ListProfile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
+import { Link } from 'react-router-dom';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Profiles } from '../../api/user/Profiles';
 import DisplayProfile from '../components/DisplayProfiles';
@@ -29,7 +30,13 @@ const ListProfile = () => {
             <h2>List Seller Profile</h2>
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4">
-            {profiles.map((profile) => (<Col key={profile._id}><DisplayProfile profile={profile} currentUser={currentUser} /> </Col>))}
+            {profiles.map((profile) => (
+              <Col key={profile._id}>
+                <DisplayProfile profile={profile} currentUser={currentUser} />
+                {/* Link to the user's profile page */}
+                <Link to={`/profile/${profile.owner}`} className="btn btn-outline-primary btn-sm custom-button" style={{ marginLeft: '10px' }}>View Profile</Link>
+              </Col>
+            ))}
           </Row>
         </Col>
       </Row>
